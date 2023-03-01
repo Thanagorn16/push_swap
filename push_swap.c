e39be40908034eaf06868e4fbf862e5f460793b0
Thanagorn Ruangsit <truangsi@z2t16c1.42bangkok.com>
2023-03-01 09:55:21 +0700

    pull

--- push_swap.c
+++ push_swap.c
@@ -2,74 +2,74 @@
 
 int main(int ac, char **av)
 {
-    int i;
-    int j;
-    int k;
-    int count;
-    int len;
-    char    **set;
-    char    **temp;
-    char    *str;
-    char    **new_args;
+	int i;
+	int j;
+	int k;
+	int count;
+	int len;
+	char    **set;
+	char    **temp;
+	char    *str;
+	char    **new_args;
 
-    if (ac < 2)
-        return (0);
+	if (ac < 2)
+		return (0);
 
 // check arguments
-    i = 1;
-    j = 0;
-    count = 0;
-    while (av[i]) // big malloc
-    {
-        j = 0;
-        new_args = ft_split(av[i], ' ');
-        while (new_args[j] != NULL)
-        {
-            j++;
-            count++; //count the set of numbers after split
-        }
-        i++;
-    }
-    set = (char **)malloc(sizeof(char *) * count);
-    i = 1;
-    j = 0;
-    while (av[i])
-    {
-        k = 0;
-        if (av[i]) //split
-            temp = ft_split(av[i], ' ');
-        while (temp[k] != NULL) // copy the splitted nums to malloc
-        {
-            str = ft_strdup(temp[k]);
-            set[j] = (char *)malloc(sizeof(char) * ft_strlen(str) + 1); // small malloc
-            len = 0;
-            while (str[len]) 
-            {
-                set[j][len] = str[len];
-                len++;
-            }
-            j++;
-            k++; // this is used to get out of the loop
-        }
-        i++;
-    }
-    i = 0;
-    while (set[i])
-        printf("%s\n", set[i++]);
-    // new_args = ft_strdup(av[i++]);
-    // while (av[i])
-    //     new_args = ft_strjoin(new_args, av[i++]);
-    // // discard spaces
-    // temp = ft_split(new_args, ' ');
-    // free(new_args);
-    // if (check_digit(temp) < 0)
-    //     return (1);
-    // // convert string ti integer
-    // i = 0;
-    // while (temp[i])
-    //     i++;
-    // printf("%d\n", i);
-    // i = 0;
-    // while(temp[i])
-    //     printf("here:%s\n", temp[i++]);
+	i = 1;
+	j = 0;
+	count = 0;
+	while (av[i]) // big malloc
+	{
+		j = 0;
+		new_args = ft_split(av[i], ' ');
+		while (new_args[j] != NULL)
+		{
+			j++;
+			count++; //count the set of numbers after split
+		}
+		i++;
+	}
+	set = (char **)malloc(sizeof(char *) * count);
+	i = 1;
+	j = 0;
+	while (av[i])
+	{
+		k = 0;
+		if (av[i]) //split
+			temp = ft_split(av[i], ' ');
+		while (temp[k] != NULL) // copy the splitted nums to malloc
+		{
+			str = ft_strdup(temp[k]);
+			set[j] = (char *)malloc(sizeof(char) * ft_strlen(str) + 1); // small malloc
+			len = 0;
+			while (str[len])
+			{
+				set[j][len] = str[len];
+				len++;
+			}
+			j++;
+			k++; // this is used to get out of the loop
+		}
+		i++;
+	}
+	i = 0;
+	while (set[i])
+		printf("%s\n", set[i++]);
+	// new_args = ft_strdup(av[i++]);
+	// while (av[i])
+	//     new_args = ft_strjoin(new_args, av[i++]);
+	// // discard spaces
+	// temp = ft_split(new_args, ' ');
+	// free(new_args);
+	// if (check_digit(temp) < 0)
+	//     return (1);
+	// // convert string ti integer
+	// i = 0;
+	// while (temp[i])
+	//     i++;
+	// printf("%d\n", i);
+	// i = 0;
+	// while(temp[i])
+	//     printf("here:%s\n", temp[i++]);
 }
